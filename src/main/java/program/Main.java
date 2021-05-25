f07c0ba55afae64dd5ce2a0b6d20f93c714b8768
praca praca <valdyrhjord@gmail.com>
2021-05-25 13:02:44 +0200

    Some tests with WeaponService.

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -6,6 +6,13 @@ import weapon.WeaponType;
 
 public class Main {
     public static void main(String[] args) {
-        
+        WeaponRepository weaponRepository = new WeaponRepository();
+        WeaponService weaponService = new WeaponService();
+
+        weaponService.addWeapon("Miecz", WeaponType.SWORD,20);
+        weaponService.showAllWeapons();
+        weaponService.editWeapon(0,"Siekiera",WeaponType.AXE,30);
+        weaponService.addWeapon("Miecz", WeaponType.SWORD,20);
+        weaponService.showAllWeapons();
     }
 }
